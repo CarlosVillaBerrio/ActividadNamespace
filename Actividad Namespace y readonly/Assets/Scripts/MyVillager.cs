16d51b61d289bd76ecd730c746b0fefd2c169a45
Carlos Fernando Villa Berrio <carlos.shizuken@gmail.com>
2019-08-30 06:15:14 -0500

    Actividad #3
    Hace lo que pide el taller.
    Falta Comentar

---
+++ Actividad Namespace y readonly/Assets/Scripts/MyVillager.cs
@@ -0,0 +1,27 @@
+﻿namespace NPC
+{
+    namespace Ally
+    {
+        using UnityEngine;
+        public struct VillagerStruct
+        {
+            // Datos del aldeano
+            public enum nombresAldeano
+            {
+                Alex, Bernardo, Carlos, David, Fernando, Gustavo, Jose, Maria, Jesus, Pedro,
+                Camilo, Alberto, Leon, John, Camila, Layla, Emily, Claire, Marta, Sofia
+            };
+            public nombresAldeano nombreAldeano;
+            public int edadAldeano;
+        }
+        public class MyVillager : MonoBehaviour
+        {
+            public VillagerStruct datosAldeano;
+            void Awake()
+            {
+                datosAldeano.nombreAldeano = (VillagerStruct.nombresAldeano)Random.Range(0, 20); // SELECTOR DE NOMBRES
+                datosAldeano.edadAldeano = Random.Range(15, 101); // SELECTOR DE EDAD
+            }
+        }
+    }
+}
